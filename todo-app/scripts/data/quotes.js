711c5b31173bc56efa02c58f484d9374e9fbfd29
Tosanwumi Abugewa-Ejegi <tosanwumiae@gmail.com>
2024-11-07 14:19:04 +0100

    Add quotes data in todo app

---
+++ todo-app/scripts/data/quotes.js
@@ -0,0 +1,27 @@
+const quotes = [
+  {
+    quote:
+      "I do not know anyone who has got to the top without hard work. That is the recipe.",
+    author: "Margaret Thatcher",
+  },
+  {
+    quote:
+      "Do the best you can until you know better. Then when you know better, do better.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The only thing standing between you and outrageous success is continuous progress you need discipline.",
+    author: "Dan Waldschmidt",
+  },
+  {
+    quote:
+      "The fight is won or lost far away from witnesses — behind the lines, in the gym, and out there on the road, long before I dance under those lights.",
+    author: "Muhammad Ali",
+  },
+  {
+    quote:
+      "Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.",
+    author: "Dale Carnegie",
+  },
+];
